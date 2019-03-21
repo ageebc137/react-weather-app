660110c34c4e3807756c6843b7ed569ad9199539
Allister <ageebernal@aol.com>
2019-03-20 23:52:50 -0700

    added api url weatheropen to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      query: "",
       city: "San Lorenzo",
       temperature: 50,
       lat: 0,
       lng: 0
     }
     this.handleInputLocation = this.handleInputLocation.bind(this);
+    this.handleSubmitButton = this.handleSubmitButton.bind(this);
     
   }
   handleInputLocation = (city) => {
     this.setState({
-      city
+      query: city
     });
+  }
+  handleSubmitButton = () => {
+    const location = this.state.query;
+    console.log(location);
+    axios.post('http://localhost:5000/getweather', {location})
+          .then((res) => {
+            console.log(res);
+          });
   }
   render() {
     return (
       <div className="App">
         {this.state.city && <City city={this.state.city}/>} 
         <Temperature temperature={this.state.temperature} />
-        <Input handleInputLocation={this.handleInputLocation}/>
+        <Input handleInputLocation={this.handleInputLocation} handleSubmitButton={this.handleSubmitButton} />
       </div>
     );
   }
@@ -55,18 +66,23 @@ class Input extends Component {
     const handleInputLocation = (e) => {
       e.preventDefault();
       this.props.handleInputLocation(e.target.value);
+    };
+
+    const handleSubmitButton = (e) => {
+      e.preventDefault();
+      this.props.handleSubmitButton();
     }
 
     return (
       <div>
         <form>
           <input onChange={handleInputLocation} placeholder="Enter city"></input>
-          <button type="submit">Search</button>
+          <button onClick={handleSubmitButton} type="submit">Search</button>
         </form>
       </div>
     )
   }
-}
+};
 
 
 
--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 require('dotenv').config();
 
+// requiring packages
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
+// Requesting express app 
 const app = express();
 
 const port = process.env.PORT || 5000;
-console.log(process.env.BING_API_KEY);
 
-
+// Use cors() in order to achieve cross origin access with React client.
 app.use(cors());
+//bodyParser allows requests made to server API to convert data to json
+app.use(bodyParser.json()); 
 
-app.get('/getweather', (req, res) => {
-  axios.get('http://dev.virtualearth.net/REST/v1/Locations?countryRegion={countryRegion}&adminDistrict={adminDistrict}&locality={locality}&postalCode={postalCode}&addressLine={addressLine}&userLocation={userLocation}&userIp={userIp}&usermapView={usermapView}&includeNeighborhood={includeNeighborhood}&maxResults={maxResults}&key={BingMapsKey}')
+app.post('/getweather', ( req, res) => {
+  const location = req.body.location;
+
+  const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_API}`;
+
+  axios.get(apiURL).then((response) => {
+    res.send(response.data)
+  });
+
 });
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
