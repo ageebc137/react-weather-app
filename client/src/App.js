a866ae17dcc6a0ae3f04ff0d9f9e67a00ec63b62
Allister <ageebernal@aol.com>
2019-03-22 17:49:39 -0700

    change app server route on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
   handleSubmitButton = () => {
     const location = this.state.query;
 
-    axios.post('http://localhost:5000/getweather', {location})
+    axios.post('https://blooming-fortress-19253.herokuapp.com/getweather', {location})
           .then((res) => {
             let temperature;
             if (this.state.tempUnit === 'Fahrenheit') {
